4a5d7ee76942abea634cec7fcd799b2eb9e449ce
ichristwin <ichristwin@gmail.com>
2024-03-19 20:32:48 +0100

    attempting fix for image issue

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,10 @@
 const nextConfig = {
   images: {
     dangerouslyAllowSVG: true,
-    remotePatterns: [{ hostname: "mqtt-node.vercel.app" }, {protocol: "http", hostname: "localhost", port: "3000"}],
   },
   async rewrites() {
     return [
       {
-        source: "/api/m3ter-head/:seed",
         destination: "/api/m3ter-head",
       },
     ];
